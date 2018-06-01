ec7a478f2b8bce28e812322b14c3b53ba8330fab
Thyago Stall <thstall@gmail.com>
2018-05-31 21:38:30 -0300

    Adds setup for packaging it

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+setup(
+    name='duolingoscoreboard',
+    packages=['scoreboard'],
+    version='v0.1',
+    description='Client for time cards APIs',
+    author='Thyago Stall',
+    author_email='thstall@gmail.com',
+    url='https://github.com/thyagostall/duolingoscoreboard',
+    download_url='https://github.com/thyagostall/duolingoscoreboard/archive/v0.1.tar.gz',
+    keywords=[],
+    classifiers=[],
+    install_requires=[
+        'certifi==2018.4.16',
+        'chardet==3.0.4',
+        'idna==2.6',
+        'requests==2.18.4',
+        'urllib3==1.22',
+    ]
+)
