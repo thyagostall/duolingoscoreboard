24bc53aec0ad7a627db46899e1ca3cbeaa9f199c
Thyago Stall <thstall@gmail.com>
2018-05-31 21:21:46 -0300

    Refactors for packaging to PyPI

--- scoreboard/scoreboard.py
+++ scoreboard/scoreboard.py
@@ -2,36 +2,28 @@ import requests
 import datetime
 
 
-def main():
-    print_user_scoreboard('<user name here>')
+def get(username):
+    return _get_json(username)
 
 
-def print_user_scoreboard(user_name):
-    user_data = get_json(user_name)
-    print(user_name)
-    print(get_improvement_grouped_by_day(user_data))
-    print(get_streak(user_data))
-    print(get_total_points(user_name, user_data))
-
-
-def get_json(username):
+def _get_json(username):
     response = requests.get(f'https://www.duolingo.com/users/{username}')
     return response.json()
 
 
-def get_improvement_grouped_by_day(user_data):
+def _get_improvement_grouped_by_day(user_data):
     last_activity = user_data['calendar']
     last_activity.sort(key=lambda x: x['datetime'])
-    last_activity = [parse_activity(a) for a in last_activity]
-    last_activity = group_by(last_activity, key_field='datetime', sum_field='improvement')
+    last_activity = [_parse_activity(a) for a in last_activity]
+    last_activity = _group_by(last_activity, key_field='datetime', sum_field='improvement')
     return last_activity
 
 
-def get_streak(user_data):
+def _get_streak(user_data):
     return user_data['site_streak']
 
 
-def get_total_points(user_name, user_data):
+def _get_total_points(user_name, user_data):
     ranking = user_data['language_data']['es']['points_ranking_data_dict']
     ranking = ranking.values()
     ranking = list(filter(lambda x: x['username'].lower() == user_name, ranking))
@@ -40,7 +32,7 @@ def get_total_points(user_name, user_data):
     return ranking
 
 
-def group_by(some_list, key_field, sum_field):
+def _group_by(some_list, key_field, sum_field):
     result = {}
     for item in some_list:
         if result.get(item[key_field]):
@@ -51,10 +43,6 @@ def group_by(some_list, key_field, sum_field):
     return result
 
 
-def parse_activity(activity):
+def _parse_activity(activity):
     occurrence_time = datetime.datetime.fromtimestamp(activity['datetime'] / 1000)
     return {'improvement': activity['improvement'], 'datetime': occurrence_time.strftime('%Y-%m-%d')}
-
-
-if __name__ == '__main__':
-    main()
